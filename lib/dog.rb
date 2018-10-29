b4ac67727dd4aa8f5f3750605bc16050173b16fe
Greg Morgan <gregm9999@gmail.com>
2018-10-29 23:53:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,8 @@
 class Dog
-  def bark
-    puts "Woof!"
-  end
-  def sit
-    puts "The Dog is sitting"
-  end
+def name=(dogs_name)
+  @this_dogs_name=dogs_name
+end
+def name
+  @this_dogs_name
+end
 end
